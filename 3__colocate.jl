d10a8e7f020b4c3028fdb5f989430955046ae56d
John Waczak <john.louis.waczak@gmail.com>
2023-08-17 08:17:38 -0500

    updated hsi saving

---
+++ 3__colocate.jl
@@ -0,0 +1,11 @@
+using RobotTeam
+
+using HDF5
+using Dates
+using CSV, DataFrames
+using HDF5
+
+include("utils/vis_tools.jl")
+include("utils/config.jl")
+
+test_path = "/media/jwaczak/LabData/RobotTeam/processed/hsi/11-23/Scotty_1/Scotty_1-1.h5"
--- src/Georectification/resample.jl
+++ src/Georectification/resample.jl
@@ -1,6 +1,6 @@
 using Statistics
 using HDF5
-
+using Measurements
 """
     bump_to_nearest_Δx(val, Δx)
 
@@ -120,6 +120,27 @@ function resample_datacube(hsi::HyperspectralImage; Δx=0.10)
         "solar_azimuth",
         "solar_elevation",
         "solar_zenith",
+        "mNDWI",
+        "NDVI",
+        "SR",
+        "EVI",
+        "AVRI",
+        "NDVI_705",
+        "MSR_705",
+        "MNDVI",
+        "VOG1",
+        "VOG2",
+        "VOG3",
+        "PRI",
+        "SIPI",
+        "PSRI",
+        "CRI1",
+        "CRI2",
+        "ARI1",
+        "ARI2",
+        "WBI",
+        "MCRI",
+        "TCARI",
     ]
 
     printnames = [
@@ -131,6 +152,27 @@ function resample_datacube(hsi::HyperspectralImage; Δx=0.10)
         "Solar Azimuth",
         "Solar Elevation",
         "Solar Zenith",
+        "Modified Normalized Difference Water Index",
+        "Normalized Difference Vegetative Index",
+        "Simple Ratio",
+        "Enhanced Vegetative Index",
+        "Atmospherically Resistant Vegetative Index",
+        "Red Edge Normalized Difference Vegetation Index",
+        "Modified Red Edge Simple Ratio",
+        "Modified Red Edge Normalized Vegetation Index",
+        "Vogelmann Red Edge Index",
+        "Vogelmann Red Edge Index 2",
+        "Vogelmann Red Edge Index 3",
+        "Photochemical Reflectance Index",
+        "Structure Intensive Pigment Index",
+        "Plant Senescence Reflectance Index",
+        "Carotenoid Reflectance Index",
+        "Carotenoid Reflectance Index 2",
+        "Anthocyanin Reflectance Index",
+        "Anthocyanin Reflectance Index 2",
+        "Water Band Index",
+        "Modified Chlorophyll Absorption Reflectance Index",
+        "Transformed Chlorophyll Absorption Reflectance Index",
     ]
 
     # 4. Allocate Data Matrices
@@ -191,7 +233,364 @@ function resample_datacube(hsi::HyperspectralImage; Δx=0.10)
     end
 
 
-    return xs_new, ys_new, hsi.isnorth, hsi.zone, Longitudes_out, Latitudes_out, IsInbounds, varnames, printnames, hsi.λs, Data_μ, Data_σ
+    # add in derived metrics
+    λs = hsi.λs
+
+    ij_pixels = findall(IsInbounds)
+
+    println("\tgenerating derived λ metrics...")
+    Threads.@threads for ij ∈ ij_pixels
+        k_λ = k_zen+1
+
+        # mNDWI
+        kgreen = findfirst(λs .> 550)
+        kswir = findfirst(λs .> 770)
+
+        green = Data_μ[kgreen,ij] ± Data_σ[kgreen,ij]
+        swir = Data_μ[kswir,ij] ± Data_μ[kswir,ij]
+
+        numer = green - swir
+        denom = green + swir
+
+        mNDWI = numer / denom
+        Data_μ[k_λ,ij] =  Measurements.value(mNDWI)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(mNDWI)
+
+        # NDVI "normalized difference vegetative index ∈ [-1, 1]"
+        k_λ += 1
+        kir = findfirst(λs .> 800)
+        kred = findfirst(λs .> 680)
+
+        ir_band = Data_μ[kir,ij] ± Data_σ[kir,ij]
+        red_band = Data_μ[kred,ij] ± Data_σ[kred,ij]
+
+        numer = (ir_band - red_band)
+        denom = (ir_band + red_band)
+
+        NDVI = numer / denom
+        Data_μ[k_λ,ij] = Measurements.value(NDVI)
+        Data_σ[k_λ,ij] = Measurements.uncertainty(NDVI)
+
+        # SR "simple ratio ∈ [0, 30]"
+        k_λ += 1
+        ir_band = Data_μ[kir,ij] ± Data_σ[kir,ij]
+        red_band = Data_μ[kred,ij] ± Data_σ[kred,ij]
+
+        numer = ir_band
+        denom = red_band
+
+        SR = numer / denom
+        Data_μ[k_λ,ij]  = Measurements.value(SR)
+        Data_σ[k_λ,ij]  = Measurements.uncertainty(SR)
+
+        # EVI "enhanced vegetative index ∈ [-1, 1]"
+        k_λ += 1
+        kblue = findfirst(λs .> 450)
+
+        ir_band = Data_μ[kir,ij] ± Data_σ[kir,ij]
+        red_band = Data_μ[kred,ij] ± Data_σ[kred,ij]
+        blue_band = Data_μ[kblue,ij] ± Data_σ[kblue,ij]
+
+        numer = 2.5 * (ir_band - red_band)
+        denom = ir_band + 6 * red_band - 7.5 * blue_band
+
+        EVI = numer / denom
+
+        Data_μ[k_λ,ij] = Measurements.value(EVI)
+        Data_σ[k_λ,ij] = Measurements.uncertainty(EVI)
+
+        # AVRI "Atmospherical Resistant Vegitative Indes"
+        k_λ += 1
+
+        ir_band = Data_μ[kir,ij] ± Data_σ[kir,ij]
+        red_band = Data_μ[kred,ij] ± Data_σ[kred,ij]
+        blue_band = Data_μ[kblue,ij] ± Data_σ[kblue,ij]
+
+        numer = (ir_band - 2 * red_band + blue_band)
+        denom = (ir_band + 2 * red_band - blue_band)
+
+        AVRI =  (numer / denom)
+
+        Data_μ[k_λ,ij] =  Measurements.value(AVRI)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(AVRI)
+
+        # # NDVI_705 "Red Edge Normalized Difference Vegetation Index"
+        k_λ += 1
+        kir = findfirst(λs .> 750)
+        kred = findfirst(λs .> 705)
+
+        ir_band = Data_μ[kir,ij] ± Data_σ[kir,ij]
+        red_band = Data_μ[kred,ij] ± Data_σ[kred,ij]
+
+        numer = (ir_band - red_band)
+        denom = (ir_band + red_band)
+
+        NDVI_705 = (numer / denom)
+
+        Data_μ[k_λ,ij] =  Measurements.value(NDVI_705)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(NDVI_705)
+
+
+        # # MSR_705 "Modified Red Edge Simple Ratio Index"
+        k_λ += 1
+        kblue = findfirst(λs .> 445)
+
+        ir_band = Data_μ[kir,ij] ± Data_σ[kir,ij]
+        red_band = Data_μ[kred,ij] ± Data_σ[kred,ij]
+        blue_band = Data_μ[kblue,ij] ± Data_σ[kblue,ij]
+
+        numer = (ir_band - blue_band)
+        denom = (red_band - blue_band)
+
+        MSR_705 = (numer / denom)
+
+        Data_μ[k_λ,ij] =  Measurements.value(MSR_705)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(MSR_705)
+
+
+        # # MNDVI "modified red edge normalized vegetation index"
+        k_λ += 1
+
+        ir_band = Data_μ[kir,ij] ± Data_σ[kir,ij]
+        red_band = Data_μ[kred,ij] ± Data_σ[kred,ij]
+        blue_band = Data_μ[kblue,ij] ± Data_σ[kblue,ij]
+
+        numer = (ir_band - red_band)
+        denom = (ir_band + red_band - 2 * blue_band)
+
+        MNDVI =  (numer / denom)
+
+        Data_μ[k_λ,ij] =  Measurements.value(MNDVI)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(MNDVI)
+
+
+        # # VOG1 "vogelmann red edge index"
+        k_λ += 1
+        kir = findfirst(λs .> 740)
+        kred = findfirst(λs .> 720)
+
+        ir_band = Data_μ[kir,ij] ± Data_σ[kir,ij]
+        red_band = Data_μ[kred,ij] ± Data_σ[kred,ij]
+
+        numer = (ir_band)
+        denom = (red_band)
+
+        VOG1 = (numer / denom)
+
+        Data_μ[k_λ,ij] =  Measurements.value(VOG1)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(VOG1)
+
+
+        # # VOG2 "vogelmann red edge index 2"
+        k_λ += 1
+        k1 = findfirst(λs .> 734)
+        k2 = findfirst(λs .> 747)
+        k3 = findfirst(λs .> 715)
+        k4 = findfirst(λs .> 726)
+
+        band1 = Data_μ[k1,ij] ± Data_σ[k1,ij]
+        band2 = Data_μ[k2,ij] ± Data_σ[k2,ij]
+        band3 = Data_μ[k3,ij] ± Data_σ[k3,ij]
+        band4 = Data_μ[k4,ij] ± Data_σ[k4,ij]
+
+        numer = (band1 - band2)
+        denom = (band3 + band4)
+
+        VOG2 =  (numer / denom)
+
+        Data_μ[k_λ,ij] =  Measurements.value(VOG2)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(VOG2)
+
+
+        # # VOG3 "vogelmann red edge index 3 ∈ [0, 20]"
+        k_λ += 1
+        k1 = findfirst(λs .> 734)
+        k2 = findfirst(λs .> 747)
+        k3 = findfirst(λs .> 715)
+        k4 = findfirst(λs .> 720)
+
+        band1 = Data_μ[k1,ij] ± Data_σ[k1,ij]
+        band2 = Data_μ[k2,ij] ± Data_σ[k2,ij]
+        band3 = Data_μ[k3,ij] ± Data_σ[k3,ij]
+        band4 = Data_μ[k4,ij] ± Data_σ[k4,ij]
+
+        numer = (band1 - band2)
+        denom = (band3 + band4)
+
+        VOG3 =  (numer / denom)
+
+        Data_μ[k_λ,ij] =  Measurements.value(VOG3)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(VOG3)
+
+
+        # # PRI "photochemical reflectance index" ∈[-1, 1]
+        k_λ
+        k1 = findfirst(λs .> 531)
+        k2 = findfirst(λs .> 570)
+
+        band1 = Data_μ[k1,ij] ± Data_σ[k1,ij]
+        band2 = Data_μ[k2,ij] ± Data_σ[k2,ij]
+
+        numer = (band1 - band2)
+        denom = (band1 + band2)
+
+        PRI = (numer / denom)
+
+        Data_μ[k_λ,ij] =  Measurements.value(PRI)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(PRI)
+
+
+
+        # # SIPI "structure intensive pigment index" ∈[0, 2]
+        k_λ += 1
+        k1 = findfirst(λs .> 800)
+        k2 = findfirst(λs .> 445)
+        k3 = findfirst(λs .> 680)
+
+        band1 = Data_μ[k1,ij] ± Data_σ[k1,ij]
+        band2 = Data_μ[k2,ij] ± Data_σ[k2,ij]
+        band3 = Data_μ[k3,ij] ± Data_σ[k3,ij]
+
+        numer = (band1 - band2)
+        denom = (band1 + band3)
+
+        SIPI = (numer / denom)
+
+        Data_μ[k_λ,ij] =  Measurements.value(SIPI)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(SIPI)
+
+
+        # # PSRI "Plant Senescence Reflectance Index"
+        k_λ += 1
+        k1 = findfirst(λs .> 680)
+        k2 = findfirst(λs .> 500)
+        k3 = findfirst(λs .> 750)
+
+        band1 = Data_μ[k1,ij] ± Data_σ[k1,ij]
+        band2 = Data_μ[k2,ij] ± Data_σ[k2,ij]
+        band3 = Data_μ[k3,ij] ± Data_σ[k3,ij]
+
+        numer = (band1 - band2)
+        denom = band3
+
+        PSRI = (numer / denom)
+
+        Data_μ[k_λ,ij] =  Measurements.value(PSRI)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(PSRI)
+
+
+        # # CRI1 "carotenoid reflectance index"
+        k_λ += 1
+        k1 = findfirst(λs .> 510)
+        k2 = findfirst(λs .> 550)
+
+        band1 = Data_μ[k1,ij] ± Data_σ[k1,ij]
+        band2 = Data_μ[k2,ij] ± Data_σ[k2,ij]
+
+        CRI1 = ((1 / band1) - (1 / band2))
+
+        Data_μ[k_λ,ij] =  Measurements.value(CRI1)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(CRI1)
+
+        # CRI2 "carotenoid reflectance index 2"
+        k_λ += 1
+        k1 = findfirst(λs .> 510)
+        k2 = findfirst(λs .> 700)
+
+        band1 = Data_μ[k1,ij] ± Data_σ[k1,ij]
+        band2 = Data_μ[k2,ij] ± Data_σ[k2,ij]
+
+        CRI2 = ((1 / band1) - (1 / band2))
+
+        Data_μ[k_λ,ij] =  Measurements.value(CRI2)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(CRI2)
+
+        # # ARI1 "anthocyanin reflectance index"
+        k_λ += 1
+        k1 = findfirst(λs .> 550)
+        k2 = findfirst(λs .> 700)
+
+        band1 = Data_μ[k1,ij] ± Data_σ[k1,ij]
+        band2 = Data_μ[k2,ij] ± Data_σ[k2,ij]
+
+        ARI1 = ((1 / band1) - (1 / band2))
+
+        Data_μ[k_λ,ij] =  Measurements.value(ARI1)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(ARI1)
+
+
+        # # ARI2 "anthocyanin reflectance index 2"
+        k_λ += 1
+        k1 = findfirst(λs .> 550)
+        k2 = findfirst(λs .> 700)
+        k3 = findfirst(λs .> 800)
+
+        band1 = Data_μ[k1,ij] ± Data_σ[k1,ij]
+        band2 = Data_μ[k2,ij] ± Data_σ[k2,ij]
+        band3 = Data_μ[k3,ij] ± Data_σ[k3,ij]
+
+        ARI2 = (band3*((1 / band1) - (1 / band2)))
+
+        Data_μ[k_λ,ij] =  Measurements.value(ARI2)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(ARI2)
+
+
+        # # WBI "water band index"
+        k_λ += 1
+        k1 = findfirst(λs .> 900)
+        k2 = findfirst(λs .> 970)
+
+        band1 = Data_μ[k1,ij] ± Data_σ[k1,ij]
+        band2 = Data_μ[k2,ij] ± Data_σ[k2,ij]
+
+        numer = band1
+        denom = band2
+
+        WBI = (numer / denom)
+
+        Data_μ[k_λ,ij] =  Measurements.value(WBI)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(WBI)
+
+
+        # # MCRI "Modified Chlorophyll Absorption Reflectance Index"
+        k_λ += 1
+        k1 = findfirst(λs .> 550)
+        k2 = findfirst(λs .> 670)
+        k3 = findfirst(λs .> 701)
+        k4 = findfirst(λs .> 780)
+
+        band1 = Data_μ[k1,ij] ± Data_σ[k1,ij]
+        band2 = Data_μ[k2,ij] ± Data_σ[k2,ij]
+        band3 = Data_μ[k3,ij] ± Data_σ[k3,ij]
+        band4 = Data_μ[k4,ij] ± Data_σ[k4,ij]
+
+        MCRI = (((band3 -band2) - 0.2 * (band3 - band1)) * (band3 / band2))
+
+        Data_μ[k_λ,ij] =  Measurements.value(MCRI)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(MCRI)
+
+
+
+        # # TCARI "transformed chlorophyll absorption reflectance index"
+        k_λ += 1
+
+        band1 = Data_μ[k1,ij] ± Data_σ[k1,ij]
+        band2 = Data_μ[k2,ij] ± Data_σ[k2,ij]
+        band3 = Data_μ[k3,ij] ± Data_σ[k3,ij]
+
+        TCARI = (3 * ((band3 - band2) - 0.2 * (band3 - band1) * (band3 /band2)))
+
+        Data_μ[k_λ,ij] =  Measurements.value(TCARI)
+        Data_σ[k_λ,ij] =  Measurements.uncertainty(TCARI)
+
+    end
+
+
+
+
+
+
+    return xs_new, ys_new, hsi.isnorth, hsi.zone, Longitudes_out, Latitudes_out, IsInbounds, varnames, printnames, λs, Data_μ, Data_σ
 
 end
 
