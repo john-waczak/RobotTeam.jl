a6dd208590e9f1bdc97bf4b53441b30a72de3ff1
John Waczak <john.louis.waczak@gmail.com>
2023-08-30 17:30:49 -0500

    added new figures for the reflectance at the arrow

--- 1e_datacube-3d-vis.jl
+++ 1e_datacube-3d-vis.jl
@@ -4,6 +4,7 @@ using BenchmarkTools
 using GLMakie, GeometryBasics
 using MintsMakieRecipes
 using ProgressMeter
+using Statistics
 
 set_theme!(mints_theme)
 
@@ -25,11 +26,19 @@ size(img)
 
 
 
+my_theme = mints_theme
+my_theme.Axis.xticklabelsize=17
+my_theme.Axis.yticklabelsize=17
+my_theme.Axis.xlabelsize=20
+my_theme.Axis.ylabelsize=20
+my_theme.Axis.titlesize=22
+set_theme!(my_theme)
+
 
 
 # 1. create visualization of un-georectified HSI
 size_in_inches = (3, 3)
-dpi = 400
+dpi = 600
 size_in_pixels = size_in_inches .* dpi
 
 fig = vis_cube(hsi; cmap=:jet, offset=0.1, ibounds=(250,1600), resolution=size_in_pixels)
@@ -45,11 +54,47 @@ xs, ys, isnorth, zone, Longitudes, Latitudes, IsInbounds, varnames, printnames, 
 
 fig = vis_rectified_cube(Data_μ, xs, ys, IsInbounds, λs, Δx; offset=0.0, ibounds=(100, length(xs)), resolution=size_in_pixels, colorbar=true)
 
-colgap!(fig.layout, -150)
+colgap!(fig.layout, -300)
 
 fig
 
-save("./paper/demo-rectified-cube.png", fig)
+save("./paper/figures/demo-rectified-cube.png", fig)
+
+
+ax = current_axis(fig)
+
+arrows!([(length(xs)-250)*Δx],[(length(ys))*Δx],[Δx*(-60)],[0],[0],[50*Δx]; arrowsize=1)
+
+
+save("./paper/figures/rectified-cube-w-arrow.png", fig)
+
+fig
+
+# plot reflectance of slice through the plume
+using CairoMakie
+
+data = log10.(Data_μ[1:462,:,:])
+idx_not_nan_or_inf = findall(.!(isnan.(data)) .&& .!(isinf.(data)))
+Rmin = quantile(data[idx_not_nan_or_inf], 0.1)
+Rmax = quantile(data[idx_not_nan_or_inf], 0.99)
+
+fig2 = Figure();
+ax = GLMakie.Axis(fig2[1,1], xlabel="λ (nm)", ylabel="Reflectance");
+ls = lines!(ax, λs, Data_μ[1:462,80, end-150], color= data[1:462,80, end-150], colormap=:jet, colorrange=(Rmin, Rmax), linewidth=3.5)
+xlims!(λs[1], λs[end])
+fig2
+
+save("./paper/figures/reflectance-at-arrow.png", fig2)
+save("./paper/figures/reflectance-at-arrow.eps", fig2)
+save("./paper/figures/reflectance-at-arrow.svg", fig2)
+save("./paper/figures/reflectance-at-arrow.pdf", fig2)
+
+
+# ij_pixels = findall(IsInbounds)
+# Ref_img = getRGB(Data_μ, λs, ij_pixels)
+# fig2, ax, hm = heatmap(xs[1]..xs[end], ys[1]..ys[end], Ref_img)
+# scatter!(ax, xs[80],ys[end-150],marker=:circle, color=:cyan, markersize=30)
+# fig2
 
 
 
